f60fe9c720beda9d92ee56db69a700ab5fb19cdd
apple <apple@appledeiMac-2.local>
2020-07-20 21:56:57 +0800

    optimize GenomicRangeQuery

--- src/codility/programmers/lessons/GenomicRangeQuery.java
+++ src/codility/programmers/lessons/GenomicRangeQuery.java
@@ -3,11 +3,6 @@
  */
 package codility.programmers.lessons;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
 /**
  * https://app.codility.com/programmers/lessons/5-prefix_sums/genomic_range_query/
  *
@@ -16,45 +11,29 @@ import java.util.List;
 public class GenomicRangeQuery {
     public int[] solution(String S, int[] P, int[] Q) {
         // write your code in Java SE 8
-        List<Integer> a = new ArrayList<>(), c = new ArrayList<>(), g = new ArrayList<>(), t = new ArrayList<>();
-        HashMap<Character, Integer> m = new HashMap<>();
-        m.put('A', 1);
-        m.put('C', 2);
-        m.put('G', 3);
-        m.put('T', 4);
-        HashMap<Character, List<Integer>> pos = new HashMap();
-        pos.put('A', new ArrayList());
-        pos.put('C', new ArrayList<>());
-        pos.put('G', new ArrayList());
-        pos.put('T', new ArrayList());
-        for (int i = 0; i != S.toCharArray().length; i++) {
-            pos.get( S.charAt(i)).add(i);
+        int l = S.length();
+        int a[] = new int[l + 1], c[] = new int[l + 1], g[] = new int[l + 1], t[] = new int[l + 1];
+        for (int i = 1; i <= S.length(); i++) {
+            char ch = S.charAt(i - 1);
+            a[i] = (ch == 'A' ? a[i - 1] + 1 : a[i - 1]);
+            c[i] = (ch == 'C' ? c[i - 1] + 1 : c[i - 1]);
+            g[i] = (ch == 'G' ? g[i - 1] + 1 : g[i - 1]);
+            t[i] = (ch == 'T' ? t[i - 1] + 1 : t[i - 1]);
         }
         int[] r = new int[P.length];
         for (int i = 0; i != P.length; i++) {
-            for (Character key : Arrays.asList('A', 'C', 'G', 'T')) {
-                if (find(P[i], Q[i], pos.get(key), 0, pos.get(key).size() - 1)) {
-                    r[i] = m.get(key);
-                    break;
-                }
+            int end = Q[i] + 1, start = P[i];
+            if (a[end] - a[start] > 0) {
+                r[i] = 1;
+            } else if (c[end] - c[start] > 0) {
+                r[i] = 2;
+            } else if (g[end] - g[start] > 0) {
+                r[i] = 3;
+            } else if (t[end] - t[start] > 0) {
+                r[i] = 4;
             }
         }
         return r;
     }
 
-    private boolean find(int left, int right, List<Integer> pos, int s, int e) {
-        if (s > e) {
-            return false;
-        }
-        if (pos.get(s) > right || pos.get(e) < left) {
-            return false;
-        }
-        int mid = (s + e) / 2;
-        if (pos.get(mid) > right) {
-            return find(left, right, pos, s, mid - 1);
-        } else if (pos.get(mid) < left) {
-            return find(left, right, pos, mid + 1, e);
-        }
-        return true;
-    }
 }
