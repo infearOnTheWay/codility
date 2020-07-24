a19232364305e935b662ad78cb349a6a6cfc701d
infearOnTheWay <infearzg@gamil.com>
2020-07-24 14:25:19 +0800

    add 'CountDiv' link

--- src/codility/programmers/lessons/Task2.java
+++ src/codility/programmers/lessons/Task2.java
@@ -1,7 +1,36 @@
+/*
+ * Copyright (C) 2020 Baidu, Inc. All Rights Reserved.
+ */
 package codility.programmers.lessons;
 
 /**
+ * https://app.codility.com/c/feedback/A6UWY5-BEB/
+ *
  * @author Zhang Gang (zhanggang02@baidu.com)
  */
-public class task2 {
+public class Task2 {
+    public int solution(String S) {
+        // write your code in Java SE 8
+        char charArray[] = S.toCharArray();
+        if (charArray.length == 0) {
+            return 0;
+        }
+        int cnt = 0, re = 0;
+        char flag = charArray[0];
+        for (char c : charArray) {
+            if (c == flag) {
+                cnt++;
+            } else {
+                re += cnt / 3;
+                flag = c;
+                cnt = 1;
+            }
+        }
+        re += cnt / 3;
+        return re;
+    }
+
+    public static void main(String args[]){
+        System.out.println(new Task2().solution("aaa"));
+    }
 }
--- src/codility/programmers/lessons/Task3.java
+++ src/codility/programmers/lessons/Task3.java
@@ -1,7 +1,37 @@
+/*
+ * Copyright (C) 2020 Baidu, Inc. All Rights Reserved.
+ */
 package codility.programmers.lessons;
+
+import java.util.HashMap;
 
 /**
  * @author Zhang Gang (zhanggang02@baidu.com)
  */
 public class Task3 {
+    public int solution(int[] A, int M) {
+        // write your code in Java SE 8
+        if (A.length == 0) {
+            return 0;
+        }
+        HashMap<Integer, Integer> idxToCnt = new HashMap<>();
+        idxToCnt.put(0, 1);
+        int max = 1;
+        for (int i = 1; i != A.length; i++) {
+            boolean addToExist = false;
+            for (int idx : idxToCnt.keySet()) {
+                if (Math.abs(A[idx] - A[i]) % M == 0) {
+                    addToExist = true;
+                    int cnt = idxToCnt.get(idx) + 1;
+                    idxToCnt.put(idx, cnt);
+                    max = Math.max(max, cnt);
+                    break;
+                }
+            }
+            if (!addToExist) {
+                idxToCnt.put(i, 1);
+            }
+        }
+        return max;
+    }
 }
